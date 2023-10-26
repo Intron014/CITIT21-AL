9b3476600874b1ccaa1f7142c71fc17eb1376701
Jorge Benjumea Burillo <me@intron014.com>
2023-10-26 16:11:12 +0200

    Ah yes, more imaginary classes

---
+++ Integer.java
@@ -0,0 +1,12 @@
+public class Integer {
+    private int valor;
+    public Integer(int valor) {
+        this.valor = valor;
+    }
+    public int getValor() {
+        return this.valor;
+    }
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
