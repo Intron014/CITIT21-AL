cc8ef43869833ec063d2417f1a836685f1f3ff6e
Jorge Benjumea Burillo <me@intron014.com>
2023-12-12 22:36:24 +0100

    Best version of Vision

--- PDF Extractor/main_vision.py
+++ PDF Extractor/main_vision.py
@@ -14,10 +14,12 @@ def find_rectangles(image):
 
     _, thresholded = cv2.threshold(img_gray, 240, 255, cv2.THRESH_BINARY_INV)
 
+    # Split the image into top and bottom halves
     height, width = img_gray.shape
     top_half = thresholded[0:height//2, :]
     bottom_half = thresholded[height//2:, :]
 
+    # Find contours in the top half
     contours_top, _ = cv2.findContours(top_half, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     rectangles_top = []
@@ -30,7 +32,9 @@ def find_rectangles(image):
             x, y, w, h = cv2.boundingRect(contour)
             if w == 1000 and h == 750:
                 rectangles_top.append((x, y, x+w, y+h))
+                print(f"Rectangle found in top half with size {w}x{h}")
 
+    # Find contours in the bottom half
     contours_bottom, _ = cv2.findContours(bottom_half, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     rectangles_bottom = []
@@ -42,8 +46,11 @@ def find_rectangles(image):
         if len(approx) == 4:
             x, y, w, h = cv2.boundingRect(contour)
             if w == 1000 and h == 750:
+                # Adjust the y coordinates for the bottom half
                 rectangles_bottom.append((x, y + height//2, x+w, y+h + height//2))
+                print(f"Rectangle found in bottom half with size {w}x{h}")
 
+    # Return the rectangles from the top half first, then the bottom half
     return rectangles_top + rectangles_bottom
 
 
@@ -55,11 +62,14 @@ def extract_images(pdf_path):
     num_pages = pdf.getNumPages()
 
     for i in range(num_pages):
+        print(f"Looking for rectangles in page {i+1}")
         images = convert_from_path(pdf_path, first_page=i+1, last_page=i+1)
         for j, image in enumerate(images):
             rectangles = find_rectangles(image)
             for k, coords in enumerate(rectangles):
+                # Crop image for the rectangle and save
                 cropped_image = image.crop(coords)
                 cropped_image.save(os.path.join(output_folder, f"output_{i*len(rectangles)+k+1}.png"))
+                print(f"Saved rectangle {k+1} from page {i+1} as output_{i*len(rectangles)+k+1}.png")
 
 extract_images(ppath)
